d44a4c607148520da34381dd0fcc4fd4672b4891
Devanshu2343 <118683912+Devanshu2343@users.noreply.github.com>
2024-06-13 22:42:33 +0530

    Add files via upload

---
+++ CipherSchools_lecture6/const.js
@@ -0,0 +1,6 @@
+// const keyword is used when we want to restrict the modification of value of variable
+
+const a = 10 ;
+console.log(a);
+
+// here we can not redeclare the value of a , since we have used const keyword 
